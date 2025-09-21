1894cc244fc7d65d047ad1ee1d8d7e4d0e22ece8
Johan Geffroy <johangeff@gmail.com>
2025-09-21 16:02:56 +0200

    Gros commit très très gros commit

---
+++ scriptcontact.js
@@ -0,0 +1,19 @@
+document.getElementById('btn-social').onclick = function() {
+  document.getElementById('modal-social').style.display = 'flex';
+};
+document.getElementById('btn-contact').onclick = function() {
+  document.getElementById('modal-contact').style.display = 'flex';
+};
+document.getElementById('close-social').onclick = function() {
+  document.getElementById('modal-social').style.display = 'none';
+};
+document.getElementById('close-contact').onclick = function() {
+  document.getElementById('modal-contact').style.display = 'none';
+};
+// Fermer la modale en cliquant en dehors
+document.getElementById('modal-social').onclick = function(e) {
+  if (e.target === this) this.style.display = 'none';
+};
+document.getElementById('modal-contact').onclick = function(e) {
+  if (e.target === this) this.style.display = 'none';
+};
